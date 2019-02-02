1d0172d7fb50d60ae9d3899d69a83b257c146204
Paul Grock <paulgrock@gmail.com>
2019-02-02 10:37:56 -0800

    Fix hero background image

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -1,15 +1,16 @@
 import PropTypes from 'prop-types'
-import React from 'react'
+import React from 'react';
 
-import styles from './hero.module.css'
+import Section from '../section-container'
+import styles from './hero.module.css';
 
 import logo from '../../images/logo-large.png';
 
 const Hero = ({ siteTitle }) => (
-	<div className={styles.heroContainer}>
+	<Section className={styles.heroContainer}>
 		<img src={logo} alt="Bird's Eye Farm" className={styles.logo}/>
 		{/* slider probably goes here */}
-	</div>
+	</Section>
 )
 
 Hero.propTypes = {
