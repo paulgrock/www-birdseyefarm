4fed8abc32ba51a1a48ebb121bbcf174443c3761
Paul Grock <paulgrock@gmail.com>
2019-02-02 21:11:17 -0800

    Switch to data for goats pages

--- src/components/goats.js
+++ src/components/goats.js
@@ -1,101 +1,64 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
+import Img from 'gatsby-image'
 
+import Section from './section-container';
 import Title from './section-title'
 
 import styles from './goats.module.css'
 
-import zadie from '../images/zadie.jpg'
-import chimi from '../images/chimi.jpg'
-
-const Goat = ({ img, children, date, title, link }) => {
+const Goat = ({ img, name, slug, date, sire, dam }) => {
   return (
     <div className={styles.goat}>
-      {/* has purple top border */}
-      <img src={img} alt={title} className={styles.photo} />
+      <Img fixed={img.childImageSharp.fixed} alt={name} className={styles.photo} />
 
       <div className={styles.content}>
         <Title>
-          <Link to={link}>{title}</Link>
+          <Link to={`/goats/${slug}`}>{name}</Link>
         </Title>
         <span>Born {date}</span>
-        <p className={styles.body}>{children}</p>
+
+        <ul className={styles.list}>
+          <li className={styles.listItem}>
+            <strong>Sire</strong>:{' '}
+            <a href={sire.link}>
+              {sire.name}
+            </a>
+          </li>
+          <li className={styles.listItem}>
+            <strong>SS</strong>: {sire.sire.name}
+          </li>
+          <li className={styles.listItem}>
+            <strong>SD</strong>: {sire.dam.name}
+          </li>
+          <li className={styles.listItem}>
+            <strong>Dam</strong>:{' '}
+            <a href={dam.link}>
+            {dam.name}
+            </a>
+          </li>
+          <li className={styles.listItem}>
+            <strong>DS</strong>: {dam.sire.name}
+          </li>
+          <li className={styles.listItem}>
+            <strong>DD</strong>: {dam.dam.name}
+          </li>
+        </ul>
       </div>
     </div>
   )
 }
 
-const Goats = props => {
+const Goats = ({goats, data}) => {
+  console.log(data)
   return (
-		<Fragment>
-      <Goat
-        img={zadie}
-        title="Harley Hillside Ginger Zinger"
-        link="/goats/harley-hillside-ginger-zinger"
-        date="2/1/2017"
-      >
-        <ul>
-          <li>
-            <strong>Sire</strong>:{' '}
-            <a href="https://curbstonevalley.com/blog/?page_id=13133">
-              Castle Rock Roxstar
-            </a>
-          </li>
-          <li>
-            <strong>SS</strong>: Caprine Acres HS Cooper
-          </li>
-          <li>
-            <strong>SD</strong>: GCH CRF Castle Rock Roxanne 1*M *D VEEE 91
-          </li>
-          <li>
-            <strong>Dam</strong>:{' '}
-            <a href="https://harleyhillsidefarm.com/ff/">
-              Harley Hillside iHeart Chai VVV+ 85
-            </a>
-          </li>
-          <li>
-            <strong>DS</strong>: Castle Rock Clark's Nutcracker
-          </li>
-          <li>
-            <strong>DD</strong>: SGCH Castle Rock Breakin My Heart 6*M VEEE 90
-          </li>
-        </ul>
-      </Goat>
-      <Goat
-        img={chimi}
-        title="Harley Hillside Dime Piece"
-        link="/goats/harley-hillside-dime-piece"
-        date="2/3/2017"
-      >
-        <ul>
-          <li>
-            <strong>Sire</strong>:{' '}
-            <a href="https://curbstonevalley.com/blog/?page_id=13133">
-              Castle Rock Roxstar
-            </a>
-          </li>
-          <li>
-            <strong>SS</strong>: Caprine Acres HS Cooper
-          </li>
-          <li>
-            <strong>SD</strong>: GCH CRF Castle Rock Roxanne 1*M
-          </li>
-          <li>
-            <strong>Dam</strong>:{' '}
-            <a href="https://curbstonevalley.com/blog/?page_id=13124">
-              Castle Rock Worth Every Penny 3*M *D AR 3059
-            </a>
-          </li>
-          <li>
-            <strong>DS</strong>: SG Castle Rock Cleveland Sage +*B *S **Elite Sire** 2015/16
-          </li>
-          <li>
-            <strong>DD</strong>: GCH Castle Rock Penny For Luck 2*M VEVV 89
-          </li>
-        </ul>
-      </Goat>
-			</Fragment>
+		<Section className={styles.container}>
+      <Title>Does</Title>
+      {goats.map(({node}) => (
+        <Goat {...node} key={node.slug} img={data[node.aka.toLowerCase()]} />
+      ))}
+			</Section>
   )
 }
 
--- src/pages/goats/index.js
+++ src/pages/goats/index.js
@@ -1,15 +1,64 @@
 import React from 'react'
-
+import { graphql } from "gatsby";
 import Layout from '../../components/layout'
 import SEO from '../../components/seo'
 
 import Goats from '../../components/goats';
 
-const GoatsPage = () => (
+const GoatsPage = ({data}) => (
   <Layout>
     <SEO title="Nigerian Dwarf Goats" />
-    <Goats />
+    <Goats goats={data.allGoatsJson.edges} data={data} />
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    allGoatsJson {
+      edges {
+        node {
+          name
+          date
+          aka
+          slug
+          sire {
+            link
+            name
+            sire {
+              name
+            }
+            dam {
+              name
+            }
+          }
+          dam {
+            link
+            name
+            sire {
+              name
+            }
+            dam {
+              name
+            }
+          }
+        }
+      }
+    }
+    zadie: file(relativePath: { eq: "harley-hillside-ginger-zinger.jpg" }) {
+      childImageSharp {
+        fixed(width: 380, height: 380) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    chimi: file(relativePath: { eq: "harley-hillside-dime-piece.jpg" }) {
+      childImageSharp {
+        fixed(width: 380, height: 380) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`;
 
 export default GoatsPage
