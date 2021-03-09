a78fd0b48054132955305b7a5845adfd8aec13b0
Paul Grock <paulgrock@gmail.com>
2021-03-09 13:15:06 -0800

    update meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	siteMetadata: {
 		title: `Bird's Eye Farm`,
-		description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+		description: `Farmstead lifestyle. Nigerian Dwarf goats, chickens, bees and Angora rabbits in Yamhill County, Oregon.`,
 		author: `@paulgrock`,
 	},
 	plugins: [
