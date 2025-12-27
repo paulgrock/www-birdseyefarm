52a8d2ceec207ff127f43f0d14f5a5bf4102c005
Paul Grock <paulgrock@gmail.com>
2025-12-26 23:02:04 -0800

    goat ass logos

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,8 @@ import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
 import './layout.css'
+import andda from '../images/andda-logo.png'
+import adga from '../images/adga-logo.avif'
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -22,7 +24,9 @@ const Layout = ({ children }) => (
         <main className="main-content">
           {children}
           <footer className="footer">
+            <img src={andda} alt="ANDDA member" height="92"  />
             © {new Date().getFullYear()} Bird’s Eye Farm | Carlton, OR
+            <img src={adga} alt="ADGA member" />
           </footer>
         </main>
       </div>
