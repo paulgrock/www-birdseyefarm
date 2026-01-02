e33ab36565ce8d588c2901b11833489ef428700d
Paul Grock <paulgrock@gmail.com>
2026-01-02 08:54:40 -0800

    switch to vanilla js for form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
   site: 'https://birdseyefarm.com',
   output: 'static',
   vite: {
